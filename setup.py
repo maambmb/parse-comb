7296b09ed1e36a60c2b37e2fa1ad3c91079bebed
maambmb <timlonsdale@gmail.com>
2016-11-15 17:40:33 +0000

    foo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='parsecomb',
+    version = '1.0.0',
+    description = 'A terse parser combinator library',
+    author_email = 'timlonsdale@gmail.com',
+    license = 'MIT',
+    keywords = 'parser combinator'
+)
