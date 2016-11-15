61b32824927f9106ab8a0708c2a4689d0c6ca5f3
maambmb <timlonsdale@gmail.com>
2016-11-15 20:25:37 +0000

    foo

--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,17 @@
-/*jshint esversion:6*/
+/* jshint esversion: 6 */
 
 var gulp = require("gulp");
-var release = require("gulp-release-it");
-var fs = require("fs");
-var ejs = require("ejs");
-var requireUncached = require("require-uncached");
-var _ = require('underscore');
+var release = require("release-it");
+var pypi = require("pypi-release");
 
-release(gulp);
+function releaseFn( increment ) {
+    release.execute( {
+        "increment" : increment,
+        "non-interactive" : true
+    } );
+    pypi.then( () => null );
+}
 
-// create a template for the setup.py file
-var TEMPLATE = ejs.compile( `
-    # auto generated setup.py script - please don't edit directly!
-    from setuptools import setup
-
-    setup(
-        name = '<%= name %>',
-        version = '<%= version %>',
-        description = '<%= description %>',
-        author_email = '<%= author %>',
-        license = '<%= license %>',
-        keywords = '<%= keywords.join( " " ) %>'
-    )
-`);
-
-var DEFAULTS = {
-    license : "MIT",
-    author : "",
-    description : "",
-    keywords : []
-};
-
-gulp.task( "mk-setup", [ "release" ], function() {
-    // load a fresh version of the node package json file
-    var pkg = _.extend( {}, DEFAULTS, requireUncached( "./package.json" ) );
-    fs.createWriteStream( "setup.py" )
-        .end( TEMPLATE( pkg ) );
-});
+gulp.task( "patch", () => releaseFn( "patch" ) );
+gulp.task( "minor", () => releaseFn( "minor" ) );
+gulp.task( "major", () => releaseFn( "major" ) );
---
+++ setup.py
@@ -0,0 +1,12 @@
+# auto generated setup.py script - please don't edit directly!
+from setuptools import setup
+
+setup(
+    name = 'parsecomb',
+    version = '1.1.0',
+    description = 'a parser combinator library',
+    author_email = 'timlonsdale@gmail.com',
+    license = 'MIT',
+    keywords = 'parser combinator',
+    packages = [ 'parsecomb' ]
+)
