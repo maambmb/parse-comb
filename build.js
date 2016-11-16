da04fb5f0c842134c8b1d83cf4a4ead9d2646e98
maambmb <timlonsdale@gmail.com>
2016-11-16 10:26:42 +0000

    foo

---
+++ build.js
@@ -0,0 +1,16 @@
+/* jshint esversion: 6 */
+
+var release = require("release-it");
+var pypi = require( "pypi-release" );
+var args = require("yargs")
+    .alias( "i", "increment" )
+    .argv;
+
+var opts = {
+    "increment" : args.increment || "patch",
+    "non-interactive" : true
+};
+
+release.execute( opts )
+    .then( pypi )
+    .then( () => null );
