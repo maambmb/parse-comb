acb2ad221ec5ecc16d946e544a37aec4c9ed3059
Tim Lonsdale <maambmb@Tims-MBP.lan>
2017-05-30 20:57:10 +0100

    again

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name             = 'parsecomb',
+    version          = '1.1.10',
+    description      = 'a parser combinator library',
+    author_email     = 'timlonsdale@gmail.com',
+    license          = 'MIT',
+    keywords         = 'parser combinator',
+    packages         = [ 'parsecomb' ],
+    install_requires = [  ]
+)
